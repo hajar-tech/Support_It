dc0ca6d4c9836deab9c38b11fd1c3c0cd9f080f5
suilahi <suilahizakaria@gmail.com>
2025-06-13 15:43:46 +0100

    ajouter relation entre ticket et panne

--- backend/src/main/java/com/itsupport/backend/models/Pannes.java
+++ backend/src/main/java/com/itsupport/backend/models/Pannes.java
@@ -1,21 +1,25 @@
 package com.itsupport.backend.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 
 import java.util.Date;
+import java.util.List;
 
 @Entity
-
+@Table(name = "Panne")
 public class Pannes {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @NotBlank(message = "Le nom est requis")
     private String Name;
+
+    @OneToMany(mappedBy = "pannes", fetch = FetchType.LAZY )
+    @JsonIgnore
+    private List<SupportTiket> supportTicketList;
+
 
     public int getId() {
         return id;
