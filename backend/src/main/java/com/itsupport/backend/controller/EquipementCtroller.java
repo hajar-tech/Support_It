0a016337ca25f789475ccf544c0af524178a631c
hajar zerhouni <hajar.zerhounipr@gmail.com>
2025-06-14 11:25:40 +0100

    modifier les information d'un equipement

--- backend/src/main/java/com/itsupport/backend/controller/EquipementCtroller.java
+++ backend/src/main/java/com/itsupport/backend/controller/EquipementCtroller.java
@@ -4,10 +4,7 @@ package com.itsupport.backend.controller;
 import com.itsupport.backend.models.Equipement;
 import com.itsupport.backend.services.EquipementService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/equipement")
@@ -25,5 +22,13 @@ public class EquipementCtroller {
     ResponseEntity<Equipement> addEquipement(@RequestBody Equipement equipment) {
        Equipement newEquipement = equipementService.addEquipement(equipment);
         return ResponseEntity.ok(newEquipement);
+    }
+
+    //update equipement
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Equipement> updateEquipement( @PathVariable Long id,  @RequestBody Equipement equipmentDetails) {
+        Equipement updatedEquipement = equipementService.updateEquipement(id, equipmentDetails);
+        return ResponseEntity.ok(updatedEquipement);
     }
 }
