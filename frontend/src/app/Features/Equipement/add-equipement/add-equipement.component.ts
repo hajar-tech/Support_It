d4e30cc3187041982484fdea3cd5fcff0661f446
hajar zerhouni <hajar.zerhounipr@gmail.com>
2025-06-15 20:59:35 +0100

    service addEquipement

---
+++ frontend/src/app/Features/Equipement/add-equipement/add-equipement.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-add-equipement',
+  imports: [
+    ReactiveFormsModule
+  ],
+  templateUrl: './add-equipement.component.html',
+  styleUrl: './add-equipement.component.css'
+})
+export class AddEquipementComponent {
+
+}
